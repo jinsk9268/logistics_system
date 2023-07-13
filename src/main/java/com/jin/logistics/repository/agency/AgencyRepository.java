7c94ffe659a7527b0c95c94a616492c0fd284f35
jinsk9268 <jinsk9268@gmail.com>
2023-07-13 22:22:45 +0900

    feat: AgencyRepository 생성

---
+++ src/main/java/com/jin/logistics/repository/agency/AgencyRepository.java
@@ -0,0 +1,10 @@
+package com.jin.logistics.repository.agency;
+
+import com.jin.logistics.domain.agency.Agency;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AgencyRepository extends JpaRepository<Agency, String> {
+
+}
