3f17a7f562b3b7de8fc8e7fa114e00d95214b1f5
jinsk9268 <jinsk9268@gmail.com>
2023-07-13 22:22:19 +0900

    feat: LogisticsAdminRepository 생성

---
+++ src/main/java/com/jin/logistics/repository/admin/LogisticsAdminRepository.java
@@ -0,0 +1,10 @@
+package com.jin.logistics.repository.admin;
+
+import com.jin.logistics.domain.admin.LogisticsAdmin;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LogisticsAdminRepository extends JpaRepository<LogisticsAdmin, String> {
+
+}
