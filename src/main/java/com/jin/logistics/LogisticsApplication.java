6ddc4d6a1da64068d9fc2b29c1b0147642b19611
jinsk9268 <jinsk9268@gmail.com>
2023-07-13 11:00:19 +0900

    feat: 공통된 테이블 컬럼에 대한 BaseEntity 생성

--- src/main/java/com/jin/logistics/LogisticsApplication.java
+++ src/main/java/com/jin/logistics/LogisticsApplication.java
@@ -2,8 +2,10 @@ package com.jin.logistics;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class LogisticsApplication {
 
   public static void main(String[] args) {
