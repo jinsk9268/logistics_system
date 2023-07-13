7ffc68b59bbd084d2ffccaebea53c1590a6af5c7
jinsk9268 <jinsk9268@gmail.com>
2023-07-13 22:25:30 +0900

    feat: OrderRepository 생성

---
+++ src/main/java/com/jin/logistics/repository/order/OrderRepository.java
@@ -0,0 +1,10 @@
+package com.jin.logistics.repository.order;
+
+import com.jin.logistics.domain.order.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+}
