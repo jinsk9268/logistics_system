1c342ef0bd3c0edcb0bb0079fbb974352e231426
jinsk9268 <jinsk9268@gmail.com>
2023-07-16 16:20:38 +0900

    refactor: Agency 컬럼 제약 조건 변경 - 데이터베이스 스키마 제약 조건 @Column 으로 변경

--- src/main/java/com/jin/logistics/domain/agency/Agency.java
+++ src/main/java/com/jin/logistics/domain/agency/Agency.java
@@ -1,44 +1,39 @@
 package com.jin.logistics.domain.agency;
 
 import com.jin.logistics.domain.BaseEntity;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
+@Builder
 public class Agency extends BaseEntity {
 
   @Id
-  @Size(max = 30)
+  @Column(length = 30)
   private String code;
-  @NotNull
-  @Size(max = 100)
+  @Column(nullable = false, length = 100)
   private String password;
-  @NotNull
-  @Size(max = 30)
+  @Column(nullable = false, length = 30)
   private String agencyName;
-  @NotNull
-  @Size(max = 50)
+  @Column(nullable = false, length = 50)
   private String email;
-  @NotNull
-  @Size(max = 30)
+  @Column(nullable = false, length = 30)
   private String ceoName;
-  @NotNull
-  @Size(max = 30)
+  @Column(nullable = false, length = 30)
   private String businessNumber;
-  @NotNull
-  @Size(max = 30)
+  @Column(nullable = false, length = 30)
   private String contact;
-  @NotNull
-  @Size(max = 1000)
+  @Column(nullable = false, length = 1000)
   private String shippingAddress;
-  @NotNull
+  @Column(nullable = false)
   private long creditLimit = 0;
 }
