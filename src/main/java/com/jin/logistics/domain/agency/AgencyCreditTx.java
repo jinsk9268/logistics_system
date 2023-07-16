8747ff861f5cbdce3e33008497ff5d170a5ee0af
jinsk9268 <jinsk9268@gmail.com>
2023-07-16 16:21:30 +0900

    refactor: AgencyCreditTx 컬럼 제약 조건 변경 - 데이터베이스 스키마 제약 조건 @Column 으로 변경

--- src/main/java/com/jin/logistics/domain/agency/AgencyCreditTx.java
+++ src/main/java/com/jin/logistics/domain/agency/AgencyCreditTx.java
@@ -1,24 +1,19 @@
 package com.jin.logistics.domain.agency;
 
 import com.jin.logistics.domain.BaseEntity;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 
 @Entity
 @Table(name = "agency_credit_transaction")
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
+@Builder
 public class AgencyCreditTx extends BaseEntity {
 
   @Id
@@ -27,8 +22,8 @@ public class AgencyCreditTx extends BaseEntity {
   @ManyToOne(optional = false)
   @JoinColumn(name = "agency_code")
   private Agency agency;
-  @NotNull
+  @Column(nullable = false)
   private long balance;
-  @Size(max = 3000)
+  @Column(length = 3000)
   private String notes;
 }
