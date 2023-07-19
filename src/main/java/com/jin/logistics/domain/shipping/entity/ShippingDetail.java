5db7f2f26c30ddfdc912f1997722795ddd9be5a9
jinsk9268 <jinsk9268@gmail.com>
2023-07-19 10:47:16 +0900

    refactor: ShippingDetail 복합키 IdClass -> Embeddable 방식으로 변경 및 공통 복합키 클래스 적용
    - (추가) vat 데이터 타입 BigDecimal로 변경

--- src/main/java/com/jin/logistics/domain/shipping/entity/ShippingDetail.java
+++ src/main/java/com/jin/logistics/domain/shipping/entity/ShippingDetail.java
@@ -1,27 +1,30 @@
-package com.jin.logistics.domain.shipping;
+package com.jin.logistics.domain.shipping.entity;
 
-import com.jin.logistics.domain.BaseEntity;
-import com.jin.logistics.domain.product.Product;
+import com.jin.logistics.domain.util.BaseEntity;
+import com.jin.logistics.domain.product.entity.Product;
+import com.jin.logistics.domain.util.DetailCompositeKey;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.math.BigDecimal;
 
 @Entity
-@IdClass(ShippingDetailPk.class)
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Builder
 public class ShippingDetail extends BaseEntity {
 
-  @Id
+  @EmbeddedId
+  private DetailCompositeKey id;
+  @MapsId("id")
   @ManyToOne(optional = false)
   @JoinColumn(name = "shipping_id")
   private Shipping shipping;
-  @Id
+  @MapsId("productCode")
   @ManyToOne(optional = false)
   @JoinColumn(name = "product_code")
   private Product product;
@@ -30,7 +33,7 @@ public class ShippingDetail extends BaseEntity {
   @Column(nullable = false)
   private long productSupplyPrice;
   @Column(nullable = false)
-  private float productVat;
+  private BigDecimal productVat;
   @Column(nullable = false)
   private long productTotalAmount;
 }
--- src/main/java/com/jin/logistics/repository/shipping/ShippingDetailRepository.java
+++ src/main/java/com/jin/logistics/repository/shipping/ShippingDetailRepository.java
@@ -1,11 +1,11 @@
 package com.jin.logistics.repository.shipping;
 
-import com.jin.logistics.domain.shipping.ShippingDetail;
-import com.jin.logistics.domain.shipping.ShippingDetailPk;
+import com.jin.logistics.domain.shipping.entity.ShippingDetail;
+import com.jin.logistics.domain.util.DetailCompositeKey;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ShippingDetailRepository extends JpaRepository<ShippingDetail, ShippingDetailPk> {
+public interface ShippingDetailRepository extends JpaRepository<ShippingDetail, DetailCompositeKey> {
 
 }
