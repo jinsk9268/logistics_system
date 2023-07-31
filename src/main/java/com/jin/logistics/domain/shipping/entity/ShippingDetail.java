3346089905f013db516474e6e2f0d49c12299c04
jinsk9268 <jinsk9268@gmail.com>
2023-07-31 14:44:07 +0900

    feat: ShippingDetail 엔티티 productVat 컬럼 기본값 정의 추가

--- src/main/java/com/jin/logistics/domain/shipping/entity/ShippingDetail.java
+++ src/main/java/com/jin/logistics/domain/shipping/entity/ShippingDetail.java
@@ -32,7 +32,7 @@ public class ShippingDetail extends BaseEntity {
   private int quantity;
   @Column(nullable = false)
   private long productSupplyPrice;
-  @Column(nullable = false)
+  @Column(nullable = false, columnDefinition = "DECIMAL(10, 1) DEFAULT 0")
   private BigDecimal productVat;
   @Column(nullable = false)
   private long productTotalAmount;
