a3139c493e367310fb1c4a6404c5aa5c95f3c897
jinsk9268 <jinsk9268@gmail.com>
2023-08-01 16:58:30 +0900

    refactor: OrderDto Request, Response로 세분화

--- src/main/java/com/jin/logistics/domain/order/dto/OrderDto.java
+++ src/main/java/com/jin/logistics/domain/order/dto/OrderDto.java
@@ -3,66 +3,91 @@ package com.jin.logistics.domain.order.dto;
 import com.jin.logistics.domain.agency.entity.Agency;
 import com.jin.logistics.domain.order.entity.Order;
 import com.jin.logistics.domain.type.OrderStatus;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Builder
 public class OrderDto {
-
-  @NotNull
-  private Long id;
-  @NotNull
-  private String agencyCode;
-  @NotNull
-  private LocalDate orderDate;
-  @NotNull
-  private LocalDate shippingDate;
-  @NotNull
-  @Size(max = 30)
-  @Builder.Default
-  private OrderStatus orderStatus = OrderStatus.PENDING;
-  private int totalQuantity;
-  private long totalSupplyPrice;
-  @NotNull
-  private BigDecimal totalVat;
-  private long totalAmount;
-  @Size(max = 3000)
-  private String notes;
-
-  public Order toEntity(Agency agency) {
-    return Order.builder()
-        .agency(agency)
-        .orderDate(orderDate)
-        .shippingDate(shippingDate)
-        .orderStatus(orderStatus)
-        .totalQuantity(totalQuantity)
-        .totalSupplyPrice(totalSupplyPrice)
-        .totalVat(totalVat)
-        .totalAmount(totalAmount)
-        .notes(notes)
-        .build();
+  @NoArgsConstructor
+  @AllArgsConstructor
+  @Getter
+  public static class OrderItem {
+    private String productCode;
+    private int quantity;
   }
 
-  public static OrderDto fromEntity(Order order) {
-    return OrderDto.builder()
-        .id(order.getId())
-        .agencyCode(order.getAgency().getCode())
-        .orderDate(order.getOrderDate())
-        .shippingDate(order.getShippingDate())
-        .orderStatus(order.getOrderStatus())
-        .totalQuantity(order.getTotalQuantity())
-        .totalSupplyPrice(order.getTotalSupplyPrice())
-        .totalVat(order.getTotalVat())
-        .totalAmount(order.getTotalAmount())
-        .notes(Optional.ofNullable(order.getNotes()).orElse(""))
-        .build();
+  @NoArgsConstructor
+  @AllArgsConstructor
+  @Getter
+  @Builder
+  public static class Request {
+    @NotBlank
+    private String agencyCode;
+    @NotNull
+    private LocalDate orderDate;
+    @NotNull
+    private LocalDate shippingDate;
+    @NotNull
+    @Size(max = 30)
+    @Builder.Default
+    private OrderStatus orderStatus = OrderStatus.PENDING;
+    private List<OrderItem> orderItems = new ArrayList<>();
+    @Size(max = 3000)
+    private String notes;
+
+    public Order toEntity(Agency agency) {
+      return Order.builder()
+          .agency(agency)
+          .orderDate(orderDate)
+          .shippingDate(shippingDate)
+          .orderStatus(orderStatus)
+          .totalVat(BigDecimal.ZERO)
+          .notes(notes)
+          .build();
+    }
+  }
+
+  @NoArgsConstructor
+  @AllArgsConstructor
+  @Getter
+  @Builder
+  public static class Response {
+    private Long id;
+    private String agencyCode;
+    private String agencyName;
+    private LocalDate orderDate;
+    private LocalDate shippingDate;
+    private OrderStatus orderStatus;
+    private int totalQuantity;
+    private long totalSupplyPrice;
+    private BigDecimal totalVat;
+    private long totalAmount;
+    private String notes;
+
+    public static OrderDto.Response from(Order order) {
+      return OrderDto.Response.builder()
+          .id(order.getId())
+          .agencyCode(order.getAgency().getCode())
+          .agencyName(order.getAgency().getAgencyName())
+          .orderDate(order.getOrderDate())
+          .shippingDate(order.getShippingDate())
+          .orderStatus(order.getOrderStatus())
+          .totalQuantity(order.getTotalQuantity())
+          .totalSupplyPrice(order.getTotalSupplyPrice())
+          .totalVat(order.getTotalVat())
+          .totalAmount(order.getTotalAmount())
+          .notes(Optional.ofNullable(order.getNotes()).orElse(""))
+          .build();
+    }
   }
 }
