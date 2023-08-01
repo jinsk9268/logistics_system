3710884cf06be7641f3c8580d4603ae8ddc11cfb
jinsk9268 <jinsk9268@gmail.com>
2023-08-01 17:03:21 +0900

    refactor: OrderDetailDto Request, Response로 세분화

--- src/main/java/com/jin/logistics/domain/order/dto/OrderDetailDto.java
+++ src/main/java/com/jin/logistics/domain/order/dto/OrderDetailDto.java
@@ -1,7 +1,10 @@
 package com.jin.logistics.domain.order.dto;
 
+import com.jin.logistics.domain.order.entity.Order;
 import com.jin.logistics.domain.order.entity.OrderDetail;
+import com.jin.logistics.domain.product.entity.Product;
 import com.jin.logistics.domain.util.DetailCompositeKey;
+import com.jin.logistics.util.VatCalculator;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,43 +13,59 @@ import lombok.NoArgsConstructor;
 import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Builder
 public class OrderDetailDto {
 
-  @NotNull
-  private Long orderId;
-  @NotNull
-  private String productCode;
-  @NotNull
-  private String name;
-  private int quantity;
-  private long productSupplyPrice;
-  @NotNull
-  private BigDecimal productVat;
-  private long productTotalAmount;
+  @NoArgsConstructor
+  @AllArgsConstructor
+  @Getter
+  @Builder
+  public static class Request {
+    private int quantity;
+    private long productSupplyPrice;
+    @NotNull
+    private BigDecimal productVat;
 
-  public OrderDetail toEntity() {
-    return OrderDetail.builder()
-        .id(new DetailCompositeKey(orderId, productCode))
-        .quantity(quantity)
-        .productSupplyPrice(productSupplyPrice)
-        .productVat(productVat)
-        .productTotalAmount(productTotalAmount)
-        .build();
+    public OrderDetail toEntity(Order order, Product product) {
+      return OrderDetail.builder()
+          .id(new DetailCompositeKey(order.getId(), product.getCode()))
+          .order(order)
+          .product(product)
+          .quantity(quantity)
+          .productSupplyPrice(productSupplyPrice)
+          .productVat(productVat)
+          .productTotalAmount(productSupplyPrice + VatCalculator.vatToLong(productVat))
+          .build();
+    }
   }
 
-  public static OrderDetailDto fromEntity(OrderDetail orderDetail) {
-    return OrderDetailDto.builder()
-        .orderId(orderDetail.getOrder().getId())
-        .productCode(orderDetail.getProduct().getCode())
-        .name(orderDetail.getProduct().getName())
-        .quantity(orderDetail.getQuantity())
-        .productSupplyPrice(orderDetail.getProductSupplyPrice())
-        .productVat(orderDetail.getProductVat())
-        .productTotalAmount(orderDetail.getProductTotalAmount())
-        .build();
+  @NoArgsConstructor
+  @AllArgsConstructor
+  @Getter
+  @Builder
+  public static class Response {
+
+    private Long orderId;
+    private String agencyCode;
+    private String agencyName;
+    private String productCode;
+    private String productName;
+    private int quantity;
+    private long productSupplyPrice;
+    private BigDecimal productVat;
+    private long productTotalAmount;
+
+    public static OrderDetailDto.Response from(OrderDetail orderDetail) {
+      return OrderDetailDto.Response.builder()
+          .orderId(orderDetail.getOrder().getId())
+          .agencyCode(orderDetail.getOrder().getAgency().getCode())
+          .agencyName(orderDetail.getOrder().getAgency().getAgencyName())
+          .productCode(orderDetail.getProduct().getCode())
+          .productName(orderDetail.getProduct().getName())
+          .quantity(orderDetail.getQuantity())
+          .productSupplyPrice(orderDetail.getProductSupplyPrice())
+          .productVat(orderDetail.getProductVat())
+          .productTotalAmount(orderDetail.getProductTotalAmount())
+          .build();
+    }
   }
 }
