ba8b2f4971723bfd817620e67ef7c0d66d7adcab
jinsk9268 <jinsk9268@gmail.com>
2023-08-01 17:04:06 +0900

    feat: 주문 id로 주문 상세내역 찾는 메서드 추가

--- src/main/java/com/jin/logistics/repository/order/OrderDetailRepository.java
+++ src/main/java/com/jin/logistics/repository/order/OrderDetailRepository.java
@@ -5,7 +5,10 @@ import com.jin.logistics.domain.util.DetailCompositeKey;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface OrderDetailRepository extends JpaRepository<OrderDetail, DetailCompositeKey> {
 
+  List<OrderDetail> findAllByOrderId(Long orderId);
 }
