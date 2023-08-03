48efd77aafb580905b483c7dc5369e4bbef3d860
jinsk9268 <jinsk9268@gmail.com>
2023-08-03 16:34:26 +0900

    feat: 주문 상세 수량 변경으로 인한 수량, 금액 변경 메서드 추가

--- src/main/java/com/jin/logistics/domain/order/entity/OrderDetail.java
+++ src/main/java/com/jin/logistics/domain/order/entity/OrderDetail.java
@@ -3,6 +3,7 @@ package com.jin.logistics.domain.order.entity;
 import com.jin.logistics.domain.product.entity.Product;
 import com.jin.logistics.domain.util.BaseEntity;
 import com.jin.logistics.domain.util.DetailCompositeKey;
+import com.jin.logistics.util.VatCalculator;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -36,4 +37,13 @@ public class OrderDetail extends BaseEntity {
   private BigDecimal productVat;
   @Column(nullable = false)
   private long productTotalAmount;
+
+  public void changeQuantity(int changeQuantity) {
+    this.quantity = changeQuantity;
+    this.productSupplyPrice = changeQuantity * (long) this.product.getBoxSupplyPrice();
+    if (!this.productVat.equals(BigDecimal.ZERO)) {
+      this.productVat = VatCalculator.multiplyIntWithVat(changeQuantity, this.product.getBoxVat());
+    }
+    this.productTotalAmount = this.productSupplyPrice + VatCalculator.vatToLong(this.productVat);
+  }
 }
