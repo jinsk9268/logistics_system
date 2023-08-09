c8d5fda9006a34c90fddacbcc7235740683b6a04
jinsk9268 <jinsk9268@gmail.com>
2023-08-09 17:10:07 +0900

    refactor: ShippingDetail 공통 엔티티 상속 적용

--- src/main/java/com/jin/logistics/domain/shipping/entity/ShippingDetail.java
+++ src/main/java/com/jin/logistics/domain/shipping/entity/ShippingDetail.java
@@ -1,39 +1,36 @@
 package com.jin.logistics.domain.shipping.entity;
 
-import com.jin.logistics.domain.util.BaseEntity;
 import com.jin.logistics.domain.product.entity.Product;
 import com.jin.logistics.domain.util.DetailCompositeKey;
+import com.jin.logistics.domain.util.DetailEntity;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.MapsId;
 import java.math.BigDecimal;
 
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
-@Builder
-public class ShippingDetail extends BaseEntity {
+public class ShippingDetail extends DetailEntity {
 
-  @EmbeddedId
-  private DetailCompositeKey id;
   @MapsId("id")
   @ManyToOne(optional = false)
   @JoinColumn(name = "shipping_id")
   private Shipping shipping;
-  @MapsId("productCode")
-  @ManyToOne(optional = false)
-  @JoinColumn(name = "product_code")
-  private Product product;
-  @Column(nullable = false)
-  private int quantity;
-  @Column(nullable = false)
-  private long productSupplyPrice;
-  @Column(nullable = false, columnDefinition = "DECIMAL(10, 1) DEFAULT 0")
-  private BigDecimal productVat;
-  @Column(nullable = false)
-  private long productTotalAmount;
+
+  @Builder
+  public ShippingDetail(
+      DetailCompositeKey id, Product product, int quantity, long productSupplyPrice,
+      BigDecimal productVat, long productTotalAmount, Shipping shipping
+  ) {
+    super(id, product, quantity, productSupplyPrice, productVat, productTotalAmount);
+    this.shipping = shipping;
+  }
 }
